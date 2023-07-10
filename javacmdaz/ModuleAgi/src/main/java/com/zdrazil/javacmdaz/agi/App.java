e4472656156876f3a80b5679957f597f7add4a99
zdraz <zdraz@192.168.1.166>
2023-07-10 08:20:49 +0200

    Added modules for Agi and Zini

---
+++ javacmdaz/ModuleAgi/src/main/java/com/zdrazil/javacmdaz/agi/App.java
@@ -0,0 +1,13 @@
+package com.zdrazil.javacmdaz.agi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Agi!" );
+    }
+}
---
+++ javacmdaz/ModuleZini/src/main/java/com/zdrazil/javacmdaz/zini/App.java
@@ -0,0 +1,13 @@
+package com.zdrazil.javacmdaz.zini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Zini!" );
+    }
+}
