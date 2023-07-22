7f48fa45af7d62d99c95892db0a7ac452decd1ea
Zina <27zdrazilova.zina@gymjh.cz>
2023-07-22 09:56:14 +0200

    modified by zina

--- javacmdaz/ModuleZini/src/main/java/com/zdrazil/javacmdaz/zini/App.java
+++ javacmdaz/ModuleZini/src/main/java/com/zdrazil/javacmdaz/zini/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Zini!" );
+        long s = 0;  
+        for (long i = 0; i <= 1500000; i++){
+            s = s + i;
+       
+       }
+               System.out.println (s);
+
     }
 }
